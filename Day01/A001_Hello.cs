230894c33027a5d98038ada9b61d5433b67eb4a0
수민 <123913969+LaniJeong@users.noreply.github.com>
2023-07-04 23:29:09 +0900

    Day01_~A002

---
+++ Day01/A001_Hello.cs
@@ -0,0 +1,7 @@
+class Hello     // class가 프로그램의 기본 단위 -> 필드(변수)나 메소드(함수)등의 프로그램 구성요소들은 반드시 class 내에 존재
+{
+    static void Main()      // Main() : 프로그램의 시작점
+    {
+        System.Console.WriteLine("Let's Start");        // WriteLine : 문자열 출력 (Console 클래스에 속하고 System이라는 네임스페이스에 속함)
+    }
+}
---
+++ Day01/A001_Hello1.cs
@@ -0,0 +1,9 @@
+using System;   // using 문으로 System
+
+class Hello
+{
+    static void Main() 
+    {
+        Console.WriteLine("Welcome");   // using System 을 위에서 썼기 때문에 생략 후 Consloe.Writeline 바로 씀
+    }
+}
